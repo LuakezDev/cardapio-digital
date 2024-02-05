38beed73d9d370ef193375c417bac2b231e52145
LuakezDev <lucianoae93@gmail.com>
2024-02-05 03:43:38 -0300

    Finalizando com props e useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import{useState} from 'react';
 import image from "./assets/hashtaurante.webp";
 import './App.css';
 import Navegacao from "./Navegacao";
+import ItemCardapio from "./ItemCardapio";
+import {pratosPrincipais, sobremesas,bebidas} from "./cardapio";
+
 
 export function App(){
+  
+  const paginaMenu = [pratosPrincipais,sobremesas,bebidas]
+  const [paginaSelecionada, alterarPaginaSelecionada] = useState(0);
 
   return <>
          <img src={image} alt="" className="capa"/>
-         <Navegacao></Navegacao>
+         <Navegacao alterarPaginaSelecionada={alterarPaginaSelecionada}/>
+         <div className="menu">
+          {paginaMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+         
+         </div>
   
   </>
 }
